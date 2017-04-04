53abba1fca20f11644b51d1d50defc43a479214f
Patrick <Patrick@ATHENA>
2017-04-03 19:59:53 -0500

    Integrated with SaneWeb framework. Yay!

---
+++ WestwoodHeadlessHunt/WestwoodHeadlessHunt/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using SaneWeb.Resources.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WestwoodHeadlessHunt.Controllers
+{
+    public static class HomeController
+    {
+        [DataBoundView("/index.html")]
+        public static Object Index(HttpListenerContext context)
+        {
+            return new Object();
+        }
+
+        [Controller("/headGallery", APIType.GET, "application/json")]
+        public static byte[] GetHeadGallery(HttpListenerContext context, String body)
+        {
+            //TODO:
+            return new byte[] { };
+        }
+
+        [Controller("/uploadHead", APIType.POST, "application/json")]
+        public static byte[] UploadHeadImage(HttpListenerContext context, String body)
+        {
+            //TODO:
+            return new byte[] { };
+        }
+    }
+}
---
+++ WestwoodHeadlessHunt/WestwoodHeadlessHunt/Program.cs
@@ -0,0 +1,42 @@
+﻿using log4net;
+using log4net.Config;
+using SaneWeb.Resources.Attributes;
+using SaneWeb.Web;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using WestwoodHeadlessHunt.Controllers;
+
+namespace WestwoodHeadlessHunt
+{
+    class Program
+    {
+        private static readonly ILog Logger = LogManager.GetLogger(typeof(Program));
+
+        public static void Main(string[] args)
+        {
+            BasicConfigurator.Configure();
+
+            Logger.Info("Starting webserver...");
+            SaneServer server = SaneServer.CreateServer(new SaneServerConfiguration(SaneServerConfiguration.SaneServerPreset.DEFAULT, Assembly.GetExecutingAssembly(), "WestwoodHeadlessHunt"));
+            server.SetErrorHandler(ErrorHandler);
+
+            server.AddController(typeof(HomeController));
+
+            server.Run();
+            Logger.Info("Webserver live!");
+
+            Console.ReadKey();
+            server.Stop();
+        }
+
+        public static void ErrorHandler(Object sender, SaneErrorEventArgs e)
+        {
+            Logger.Error("Unexpected error - " + e.Exception.Message + e.Exception.StackTrace);
+            e.Propogate = false;
+        }
+    }
+}
